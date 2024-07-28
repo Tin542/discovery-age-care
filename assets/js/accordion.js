09173e08078d3ef314030d26a32f7bf689409b51
hoangphuc28 <zhoangp@gmail.com>
2024-07-28 11:22:52 +0700

    fix(about): fix

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,14 +1,18 @@
 document.querySelectorAll('.accordion-button').forEach(button => {
     button.addEventListener('click', () => {
         const accordionContent = button.nextElementSibling;
-        const icon = button.querySelector('i');
+        const isActive = button.classList.contains('active');
 
-        button.classList.toggle('active');
+        // Đóng tất cả các accordion
+        document.querySelectorAll('.accordion-button').forEach(btn => {
+            btn.classList.remove('active');
+            btn.nextElementSibling.style.display = 'none';
+        });
 
-        if (button.classList.contains('active')) {
+        // Nếu accordion hiện tại không hoạt động, hãy mở nó
+        if (!isActive) {
+            button.classList.add('active');
             accordionContent.style.display = 'flex';
-        } else {
-            accordionContent.style.display = 'none';
         }
     });
 });
